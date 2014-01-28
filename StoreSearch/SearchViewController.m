69cf267b804f471ce9b811a58812a79aae5d9476
tylacock <tylacock@gmail.com>
2014-01-28 14:23:45 -0500

    pg 23

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -8,12 +8,21 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController ()
+@interface SearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+// Temp stuff
+@property (strong, nonatomic)NSMutableArray *searchResults;
 
 @end
 
 @implementation SearchViewController
 
+///////////////////////////////////////////////////////////////////////////////
+#pragma mark -  Life Cycle
+///////////////////////////////////////////////////////////////////////////////
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -26,7 +35,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +45,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+#pragma mark -  Table View and Data Source
+///////////////////////////////////////////////////////////////////////////////
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.searchResults == nil) {
+        return 0;
+    } else {
+        return [self.searchResults count];
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"SearchResultCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = self.searchResults[indexPath.row];
+    
+    return cell;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+#pragma mark -  Search Bar
+///////////////////////////////////////////////////////////////////////////////
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    self.searchResults = [NSMutableArray arrayWithCapacity:20];
+    
+    for (int i = 0; i < 3; i++) {
+        [self.searchResults addObject:[NSString stringWithFormat:@"Fake result %d for '%@'", i, searchBar.text]];
+    }
+    
+    [self.tableView reloadData];
+}
 @end
