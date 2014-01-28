07c5a7d8b005a66b255e6015441a41c4fa730764
tylacock <tylacock@gmail.com>
2014-01-28 13:02:32 -0500

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Lacock, Ty on 1/28/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Lacock, Ty on 1/28/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
